129b36c64a0e0586a586fffb485a102e19578b46
mitchellkim94 <mitchellkim94@icloud.com>
2022-12-17 20:00:33 +0900

    Intersection Observer 수정

--- src/components/hooks/useTitleObserver.ts
+++ src/components/hooks/useTitleObserver.ts
@@ -5,19 +5,20 @@ import { pageTitle } from "../../store/page";
 export default <T>(ref: RefObject<T> | null, title?: string) => {
     const titleSetter = useSetRecoilState(pageTitle);
 
-    const io = new IntersectionObserver(
-        ([entry]) => {
-            if (entry.isIntersecting) {
-                titleSetter(title || '');
-            }
-        },
-        {
-            root: null,
-            threshold: [0.4, 1]
-        }
-    );
-
+    
     useEffect(() => {
+        const io = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    titleSetter(title || '');
+                }
+            },
+            {
+                root: null,
+                threshold: [0.4, 1]
+            }
+        );
+        
         io.observe(ref?.current as Element);
 
         return () => {
